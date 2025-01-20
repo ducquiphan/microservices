66ce9a3e296b024c340cfb222a0575e41326f22d
Duc Phan <phanquiduc@gmail.com>
2025-01-20 19:02:40 +0700

    Initiate restful-web-services/03-path-param

--- src/main/java/com/ducpq/rest/microservices/controller/HelloWorldController.java
+++ src/main/java/com/ducpq/rest/microservices/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.ducpq.rest.microservices.controller;
 
 import com.ducpq.rest.microservices.entity.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,6 +24,11 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping("/hello-world/{name}")
+	public HelloWorldBean helloWorldBeanName(@PathVariable(name = "name") String name) {
+		return new HelloWorldBean("Hello " + name);
 	}
 	
 }
