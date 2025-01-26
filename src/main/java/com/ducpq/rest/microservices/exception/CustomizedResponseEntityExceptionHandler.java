ea8b7de55748a1ebad2f94993dfea02799f3c24b
Duc Phan <phanquiduc@gmail.com>
2025-01-26 17:06:45 +0700

    Add: CustomizedResponseEntityExceptionHandler.java, ErrorDetails.java
    Implements custom Exception functions with REST format for all exception

---
+++ src/main/java/com/ducpq/rest/microservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.ducpq.rest.microservices.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+/**
+ * CustomizedResponseEntityExceptionHandler
+ *
+ * @author Admin
+ * @version 1.0
+ * @since 2025-01-26
+ */
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<?> handleAllExceptions(Exception ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<?> handleUserNotFoundExceptions(Exception ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+}
---
+++ src/main/java/com/ducpq/rest/microservices/exception/ErrorDetails.java
@@ -0,0 +1,46 @@
+package com.ducpq.rest.microservices.exception;
+
+import java.time.LocalDateTime;
+
+/**
+ * ErrorDetails
+ *
+ * @author Admin
+ * @version 1.0
+ * @since 2025-01-26
+ */
+public class ErrorDetails {
+	private LocalDateTime timestamp;
+	private String message;
+	private String description;
+	
+	public ErrorDetails(LocalDateTime timestamp, String message, String description) {
+		this.timestamp = timestamp;
+		this.message = message;
+		this.description = description;
+	}
+	
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
