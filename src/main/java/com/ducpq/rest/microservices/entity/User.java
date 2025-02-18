280dad17b9a1f48ad1597bf5478eb883f5516a8a
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-02-18 15:57:40 +0700

    Add MySQL database connection with Docker Update: - application.properties - pom.xml - User.java

--- src/main/java/com/ducpq/rest/microservices/entity/User.java
+++ src/main/java/com/ducpq/rest/microservices/entity/User.java
@@ -2,6 +2,8 @@ package com.ducpq.rest.microservices.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -28,8 +30,10 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	@Size(min = 2, message = "Name must be at least 2 characters long")
+	@NotEmpty(message = "Name cannot be empty")
 	private String name;
 	@Past(message = "Birth date must be in the past")
+	@NotNull(message = "Birth date cannot be null")
 	private LocalDate birthDate;
 	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
 	@JsonIgnore
