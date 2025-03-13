eebff981256b84c638f5d0bd9d260fcb27f7babc
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-13 13:32:26 +0700

    Initial

--- src/main/java/com/ducpq/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/ducpq/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.ducpq.microservices.currencyexchangeservice.controller;
 
 import com.ducpq.microservices.currencyexchangeservice.entity.CurrencyExchange;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,18 @@ import java.math.BigDecimal;
  */
 @RestController
 @RequestMapping("/currency-exchange")
+@AllArgsConstructor
 public class CurrencyExchangeController {
+	
+	private Environment environment;
+	
 	@GetMapping("/from/{fromCurrency}/to/{toCurrency}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable("fromCurrency") String fromCurrency,
 												  @PathVariable("toCurrency") String toCurrency){
-		return new CurrencyExchange(1000L, fromCurrency, toCurrency,
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, fromCurrency, toCurrency,
 				BigDecimal.valueOf(50));
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		return currencyExchange;
 	}
 }
