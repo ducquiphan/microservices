1027a670108d7f956495b11fe1a0a2dde014f36f
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-15 14:31:36 +0700

    Configure JPA and Initialized Data

--- src/main/java/com/ducpq/microservices/currencyexchangeservice/entity/CurrencyExchange.java
+++ src/main/java/com/ducpq/microservices/currencyexchangeservice/entity/CurrencyExchange.java
@@ -1,6 +1,7 @@
 package com.ducpq.microservices.currencyexchangeservice.entity;
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -17,9 +18,14 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @ToString
+@Entity
 public class CurrencyExchange {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "currency_from")
 	private String from;
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
