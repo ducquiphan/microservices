6924a45f569c624536ee9d671eb57610cb7ffebe
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2025-03-19 18:53:01 +0700

    Initial Commit

--- src/main/java/com/ducpq/mircoservice/namingserver/NamingServerApplication.java
+++ src/main/java/com/ducpq/mircoservice/namingserver/NamingServerApplication.java
@@ -1,4 +1,4 @@
-package com.ducpq.mircoservice.naming_server;
+package com.ducpq.mircoservice.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ducpq/mircoservice/namingserver/NamingServerApplicationTests.java
+++ src/test/java/com/ducpq/mircoservice/namingserver/NamingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ducpq.mircoservice.naming_server;
+package com.ducpq.mircoservice.namingserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
